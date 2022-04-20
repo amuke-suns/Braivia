381433ca2f70b7be4981cea2d5dd9b8bfdf8d8e9
Amuke Sunday <sundayamuke7@gmail.com>
2022-04-20 01:06:53 +0100

    feat: Service locator and MultiProvider now properly set up.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:braivia/screens/screens.dart';
+import 'package:braivia/services/service_locator.dart';
+import 'package:braivia/viewmodels/trivia_viewmodel.dart';
+import 'package:braivia/viewmodels/viewmodels.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BraiviaApp());
 }
 
@@ -11,17 +16,23 @@ class BraiviaApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Braivia',
-      theme: ThemeData.dark().copyWith(
-        textTheme: ThemeData.dark().textTheme.apply(
-          fontFamily: 'SecularOne',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Braivia',
+        theme: ThemeData.dark().copyWith(
+          textTheme: ThemeData.dark().textTheme.apply(
+                fontFamily: 'SecularOne',
+              ),
+          primaryTextTheme: ThemeData.dark().textTheme.apply(
+                fontFamily: 'SecularOne',
+              ),
         ),
-        primaryTextTheme: ThemeData.dark().textTheme.apply(
-          fontFamily: 'SecularOne',
-        ),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
